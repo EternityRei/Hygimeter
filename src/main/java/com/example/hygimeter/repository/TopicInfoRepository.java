a9bb187a63bbb1a9677bb5538494fa32860dfe08
EternityRei <anastasiamayorova2003@gmail.com>
2023-11-23 01:07:44 +0200

    Add query

--- src/main/java/com/example/hygimeter/repository/TopicInfoRepository.java
+++ src/main/java/com/example/hygimeter/repository/TopicInfoRepository.java
@@ -3,6 +3,8 @@ package com.example.hygimeter.repository;
 import com.example.hygimeter.model.Theme;
 import com.example.hygimeter.model.TopicInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +12,7 @@ import java.util.Optional;
 
 @Repository
 public interface TopicInfoRepository extends JpaRepository<TopicInfo, Integer> {
-    List<TopicInfo> findAllByThemes(List<Theme> themeList);
+    @Query("SELECT t FROM TopicInfo t JOIN t.themes theme WHERE theme IN :themes")
+    List<TopicInfo> findAllByThemes(@Param("themes") List<Theme> themes);
     Optional<TopicInfo> findTopicInfoById(Integer id);
 }

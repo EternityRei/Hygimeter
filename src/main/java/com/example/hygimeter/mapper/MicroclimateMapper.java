c2c06dd6af3184a74d1d8d501a72c046c8e657c5
EternityRei <anastasiamayorova2003@gmail.com>
2023-11-23 02:16:39 +0200

    Add microclimate service implementation

--- src/main/java/com/example/hygimeter/mapper/MicroclimateMapper.java
+++ src/main/java/com/example/hygimeter/mapper/MicroclimateMapper.java
@@ -5,10 +5,15 @@ import com.example.hygimeter.model.Microclimate;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface MicroclimateMapper {
     MicroclimateMapper INSTANCE = Mappers.getMapper(MicroclimateMapper.class);
 
     Microclimate toMicroclimate(MicroclimateDTO microclimateDTO);
     MicroclimateDTO toMicroclimateDTO(Microclimate microclimate);
+
+    List<Microclimate> toMicroclimates(List<MicroclimateDTO> microclimateDTOS);
+    List<MicroclimateDTO> toMicroclimateDTOS(List<Microclimate> microclimates);
 }
---
+++ src/main/java/com/example/hygimeter/service/MicroclimateServiceImpl.java
@@ -0,0 +1,64 @@
+package com.example.hygimeter.service;
+
+import com.example.hygimeter.dto.MicroclimateDTO;
+import com.example.hygimeter.mapper.MicroclimateMapper;
+import com.example.hygimeter.model.Microclimate;
+import com.example.hygimeter.model.PlanParameters;
+import com.example.hygimeter.model.PlanPattern;
+import com.example.hygimeter.repository.MicroclimateRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MicroclimateServiceImpl implements MicroclimateService{
+
+    private final MicroclimateRepository microclimateRepository;
+    private final MicroclimateMapper microclimateMapper;
+
+    @Override
+    public MicroclimateDTO createMicroclimate(MicroclimateDTO microclimateDTO) {
+        Microclimate microclimate = microclimateMapper.toMicroclimate(microclimateDTO);
+        return microclimateMapper.toMicroclimateDTO(microclimateRepository.save(microclimate));
+    }
+
+    @Override
+    public MicroclimateDTO updateMicroclimate(MicroclimateDTO microclimateDTO) {
+        Microclimate microclimate = microclimateRepository.findById(microclimateDTO.getId())
+                .orElseThrow(() -> new EntityNotFoundException("Microclimate not found"));
+
+        Microclimate newMicroclimate = microclimateMapper.toMicroclimate(microclimateDTO);
+        microclimate.setTemperature(newMicroclimate.getTemperature());
+        microclimate.setVentilation(newMicroclimate.getVentilation());
+        microclimate.setLightLevel(newMicroclimate.getLightLevel());
+        microclimate.setPlan(newMicroclimate.getPlan());
+        microclimate.setHumidity(newMicroclimate.getHumidity());
+
+        return microclimateMapper.toMicroclimateDTO(microclimateRepository.save(microclimate));
+    }
+
+    @Override
+    public void deleteMicroclimate(Integer id) {
+        Microclimate microclimate = microclimateRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Microclimate not found"));
+
+        microclimateRepository.deleteById(microclimate.getId());
+    }
+
+    @Override
+    public MicroclimateDTO getMicroclimateById(Integer id) {
+        Microclimate microclimate = microclimateRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Microclimate not found"));
+
+        return microclimateMapper.toMicroclimateDTO(microclimate);
+    }
+
+    @Override
+    public List<MicroclimateDTO> getAllMicroclimates() {
+        List<Microclimate> microclimates = microclimateRepository.findAll();
+        return microclimateMapper.toMicroclimateDTOS(microclimates);
+    }
+}
